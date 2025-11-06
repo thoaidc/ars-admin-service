aac8a8ccc0dd4dd3222dc092a9179916550f0223
thoaidc <dcthoai1023@gmail.com>
2025-11-06 21:22:49 +0700

    init

---
+++ src/main/java/com/ars/adminservice/constants/ResultConstants.java
@@ -0,0 +1,17 @@
+package com.ars.adminservice.constants;
+
+import com.dct.model.dto.response.BaseResponseDTO;
+
+/**
+ * Message in api response with internationalization (I18n) here <p>
+ * Use when you want to create a detailed response message for the client in {@link BaseResponseDTO} <p>
+ * The constant content corresponds to the message key in the resources bundle files in directories such as:
+ * <ul>
+ *   <li><a href="">resources/i18n/messages</a></li>
+ * </ul>
+ *
+ * @author thoaidc
+ */
+public interface ResultConstants {
+
+}
